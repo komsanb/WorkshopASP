9e7d6403cefaef828907d65d1a904afb3b3baf2e
komsanb <s5530213017@phuket.psu.ac.th>
2016-02-24 04:00:50 +0700

    Update for Workshop can config by students -------------------------------------------------------- commit by komsan.b

---
+++ WorkshopASP/WorkshopASP/javaScript/angular/app.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('myApp', ['ngRoute', 'ngSanitize']);
+
+app.config(['$routeProvider',
+       function ($routeProvider) {
+           $routeProvider.
+               when('/', {
+                   templateUrl: 'Views/home.html',
+               }).
+               when('/data', {
+                   templateUrl: 'Views/tables.html'
+               }).
+              otherwise({
+                  redirectTo: '/login.html'
+              });
+       }]);
+
